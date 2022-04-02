6f0f67e043de7bd492ee9fccbeadfd4768c192a4
PJ Olender <olenderpj@gmail.com>
2022-04-02 22:22:57 +0000

    Updates to home page to limit viewing possibilities for non-authenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :set_time_zone, if: :user_signed_in?
+    #before_action :set_time_zone, if: :user_signed_in?
   
-    private
-        def set_time_zone
-            Time.zone = current_user.time_zone
-        end
+   # private
+   #     def set_time_zone
+   #         Time.zone = current_user.time_zone
+   #     end
 end
