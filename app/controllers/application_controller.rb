c0a71df48a08783d43a25a4a87dfb23425663ce9
alexcgarduno <agardun2@uccs.edu>
2022-04-06 02:05:24 +0000

    Final code for calendar/meetings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   
     private
         def set_time_zone
-            Time.zone = current_user.time_zone
+            Time.zone = current_user.remember_created_at
         end
 end
--- features/step_definitions/calendar_view_steps.rb
+++ features/step_definitions/calendar_view_steps.rb
@@ -1,19 +1,36 @@
-Given(/^I am on the Read-it home page$/) do
-    visit root_path
-end
+require "simple_calendar/month_calendar"
 
 When(/^I click on the calendar button$/) do
-    click_on "view_calendar_button"
+    visit '/calendar'
 end
 
-Then(/^I should be on the calendar page$/) do
-    expect(page).to have_content("Your Reading Calendar")
+Then(/^I should be on the calendar page$/) do 
+    today = Date.today
+    expect(page).to have_content(today)
+    expect(page).to have_content(today.beginning_of_month)
+    expect(page).to have_content(today.end_of_month)
+end
 
-    #input options
-    #expect(page).to have_content(date.today.strftime(%Y, %m, %d)))
+Given('I am on the calendar page') do
+    visit '/calendar'
+end
 
-    #input fields
-    expect(page).to have_field("calendar-date")
+Then('I should see a create event button') do
+    expect(page). to have_content("Create New Event")
+end
+
+When (/^I click on the creat event button$/) do
+    visit "meetings/new"
+end
+
+Then('I should be on the create event page') do
+    expect(page). to have_content("New Event")
+    expect(page).to have_field("meeting_name")
+    expect(page).to have_field("meeting_start_time_1i")
+    expect(page).to have_field("meeting_start_time_2i")
+    expect(page).to have_field("meeting_start_time_3i")
+    expect(page).to have_field("meeting_start_time_4i")
+    expect(page).to have_field("meeting_start_time_5i")
 end
     
 
