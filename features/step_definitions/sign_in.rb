195b3b712cca2a6230daf04c726e334784addf75
PJ Olender <olenderpj@gmail.com>
2022-04-27 03:57:25 +0000

    working cucumebr tests

--- features/step_definitions/sign_in.rb
+++ features/step_definitions/sign_in.rb
@@ -1,25 +1,28 @@
-When (/^the user is on the homepage$/)
+When (/^the user is on the homepage$/) do
     visit root_path
     expect(page).to have_content "Login"
     expect(page).to have_content "Register"
+end
 
-And (/^they click on "Sign-In"$/)
+And (/^they click on "Sign-In"$/) do
     visit new_user_session_path
+end
 
-Then (/^the should be redirected to the "Sign-In Form"$/)
+Then (/^the should be redirected to the "Sign-In Form"$/) do
     expect(page).to have_content "Sign In"
     expect(page).to have_content "Email"
     expect(page).to have_content "Password"
     expect(page).to have_content "Log in"
     expect(page).to have_content "Forgot Password?"
     expect(page).to have_content "Home"
-
-When (/^the user successfully signs in with valid credentials$/)
-
-Then (/^the user should be directed to the home page$/)
-
-And (/^the user should see a welcome message of "Manage My Account"$/)
-
-When (/^the user enters their invlaid credentials$/)
-
-Then (/^they should see an invalid credentials Error message pop up$/)
+end
+When (/^the user successfully signs in with valid credentials$/) do
+end
+Then (/^the user should be directed to the home page$/) do
+end
+And (/^the user should see a welcome message of "Manage My Account"$/) do
+end
+When (/^the user enters their invlaid credentials$/) do
+end
+Then (/^they should see an invalid credentials Error message pop up$/) do
+end
