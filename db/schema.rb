cb16ed5d6e5f63f8643e005f31e69b7ada748e16
PJ Olender <olenderpj@gmail.com>
2022-04-27 03:51:45 +0000

    Reverting to working commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_04_19_003337) do
 
   create_table "books", force: :cascade do |t|
