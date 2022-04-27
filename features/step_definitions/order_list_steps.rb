97f02426e0bc0c1029d0b04c6908e962c7c1ce51
Minhaj-Rahat <rahat.minhajul@gmail.com>
2022-04-27 05:29:21 +0000

    testv1

--- features/step_definitions/order_list_steps.rb
+++ features/step_definitions/order_list_steps.rb
@@ -1,8 +1,19 @@
 Given('the following books exist:') do |table|
+  @user = User.create!({
+        :email => "none4@nomail.com",
+        :password => "12345678913",
+        :password_confirmation => "12345678913"
+      })
+      visit new_user_session_path
+
+      fill_in "Email", with: "none3@nomail.com"
+      fill_in "Password", with: "12345678913"
+      click_button "Log in"
   # table is a Cucumber::MultilineArgument::DataTable
   # Book Model is pending, not created
   table.hashes.each do |book|
     Book.create!(:title => book['title'],
+    :user => @user,
       :author => book['author'],
       :genre => book['genre'])
   end
