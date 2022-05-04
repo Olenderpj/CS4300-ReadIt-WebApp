b3c3a377fe96687a6f4e1e5b9dceb45e29869c1e
PJ Olender <olenderpj@gmail.com>
2022-05-04 17:15:08 +0000

    Correctly changed the application.rb file to not pre-compile assets

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
-# added this
-config.assets.initialize_on_precompile = false
+
 
 require_relative 'boot'
 
@@ -13,6 +12,9 @@ module ReadIt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    # added this
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
